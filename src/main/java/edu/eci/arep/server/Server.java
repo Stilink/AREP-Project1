f7487052d64b811ea9c8d70207b4c8fdb50421f1
Stilink <jeymar.vega@mail.escuelaing.edu.co>
2019-09-05 17:10:36 -0500

    Heroku

--- src/main/java/edu/eci/arep/server/Server.java
+++ src/main/java/edu/eci/arep/server/Server.java
@@ -143,6 +143,6 @@ public class Server {
 		if (System.getenv("PORT") != null) {
 			return Integer.parseInt(System.getenv("PORT"));
 		}
-		return 40567; // returns default port if heroku-port isn't set (i.e. on localhost)
+		return 4567; // returns default port if heroku-port isn't set (i.e. on localhost)
 	}
 }
