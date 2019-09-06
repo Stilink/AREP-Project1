bce5ff83a2f167ad56b6bc983a11f269ae0f8f4d
Jeymar Vega <jeymar.vega@mail.escuelaing.edu.co>
2019-09-06 16:16:18 -0500

    Create TestExtra.java

---
+++ src/main/java/edu/eci/arep/apps/TestExtra.java
@@ -0,0 +1,13 @@
+package edu.eci.arep.apps;
+
+import edu.eci.arep.server.Web;
+
+/**
+ * WebServiceHello
+ */
+public class TestExtra {
+    @Web("extra")
+    public static String square() {
+        return "<!DOCTYPE html> <html> <head> <title> Titulo </title>  </head> <body> <h1>Hello world! </h1> </body> </html>";
+    }
+ }
