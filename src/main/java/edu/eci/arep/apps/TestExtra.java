19316deb2d9ae54ed9a7d4d5e9b144265f571fb7
Jeymar Vega <jeymar.vega@mail.escuelaing.edu.co>
2019-09-06 16:16:44 -0500

    Update TestExtra.java

--- src/main/java/edu/eci/arep/apps/TestExtra.java
+++ src/main/java/edu/eci/arep/apps/TestExtra.java
@@ -8,6 +8,6 @@ import edu.eci.arep.server.Web;
 public class TestExtra {
     @Web("extra")
     public static String square() {
-        return "<!DOCTYPE html> <html> <head> <title> Titulo </title>  </head> <body> <h1>Hello world! </h1> </body> </html>";
+        return "<!DOCTYPE html> <html> <head> <title> Titulo </title>  </head> <body> <h1>Hello world! </h1> <p>Extra</p> </body> </html>";
     }
  }
